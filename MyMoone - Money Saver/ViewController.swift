d43939b0506ee31c10826d03554df8075781a1e9
Reski Arianto <reski.arianto@student.uty.ac.id>
2021-05-13 23:40:26 +0700

    Initial Commit

---
+++ MyMoone - Money Saver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMoone - Money Saver
+//
+//  Created by MacBook on 13/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
